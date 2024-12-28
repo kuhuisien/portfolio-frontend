83baa79030e4ed364a21c6d868c9248ee738403d
kuhuisien <65377775+kuhuisien@users.noreply.github.com>
2024-12-28 12:05:05 +0800

    fix background image display

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       className={
         isDarkMode
           ? ""
-          : "bg-[url('/dist/assets/lightBackground.jpg')] bg-cover bg-center bg-no-repeat h-full w-full"
+          : "bg-[url('/public/lightBackground.jpg')] bg-cover bg-center bg-no-repeat h-full w-full"
       }
     >
       <div className="flex flex-col items-center justify-center min-h-screen p-6">
