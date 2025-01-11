bca9092df5d64fb469a52e4d91ced48a8f6204c6
kuhuisien <65377775+kuhuisien@users.noreply.github.com>
2025-01-11 12:13:59 +0800

    clean up

--- src/common/components/ThemeToggle.jsx
+++ src/common/components/ThemeToggle.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import { ThemeContext } from "../contexts/ThemeContext";
 
